3561de7ab76528e81cb439f4ba31eb4eda36b58f
AimLeon <139550682+AimLeon@users.noreply.github.com>
2023-07-14 14:15:29 +0300

    boot

--- settings/ayarlar.js
+++ settings/ayarlar.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-    TOKEN: process.env.TOKEN || "TOKEN GİR",  // bot token gir :)
+    TOKEN: process.env.TOKEN || "MTEyOTM2MDE1MzM4MTE5MTczMQ.GamVek.b1Sqhqs1trMIGb-wRW7sqMrJPK4InIbJaKPgoo",  // bot token gir :)
     PREFIX: process.env.PREFIX || "!", // bot prefix
-    SAHİP: process.env.SAHİP || "SAHİP İD", //sahip id :()
+    SAHİP: process.env.SAHİP || "824671449767608340", //sahip id :()
 }
